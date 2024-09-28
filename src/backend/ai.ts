00990b406649a9fd946e2d9fe0627dbc36d70227
NikodemMarek <nikodemmarek11@gmail.com>
2024-09-29 01:44:37 +0200

    feat: streamed prompts

--- src/backend/ai.ts
+++ src/backend/ai.ts
@@ -1,60 +1,96 @@
-import { Ollama } from "ollama";
+import { ChatResponse, Ollama } from "ollama";
 
 const ollama = new Ollama({ host: "http://hy24.plasny.one:80" });
-const CONFIG = {
-    model: "llama3.2",
-};
 
-async function ask(recipe: string, prompt: string, json = false) {
-    return (await ollama.chat({
-        ...CONFIG,
-        format: json ? "json" : "text",
-        messages: [
-            {
-                role: "system",
-                content: recipe,
-            },
-            {
-                role: "user",
-                content: prompt,
-            },
-        ]
-    })).message.content
+async function ask(recipe: string, prompt: string, config: {} = {}) {
+	return (
+		await ollama.chat({
+			model: "llama3.2",
+			...config,
+			messages: [
+				{
+					role: "system",
+					content: recipe,
+				},
+				{
+					role: "user",
+					content: prompt,
+				},
+			],
+		})
+	).message.content;
 }
 
-export async function evaluate_urls(prompt: string, urls: string[]): Promise<string[]> {
-    const recipe = (await import(`./prompts/url_evaluation.md?raw`)).default;
+async function ask_stream(recipe: string, prompt: string, config: {} = {}) {
+	return await ollama.chat({
+		model: "llama3.2",
+		stream: true,
+		...config,
+		messages: [
+			{
+				role: "system",
+				content: recipe,
+			},
+			{
+				role: "user",
+				content: prompt,
+			},
+		],
+	});
+}
 
-    const response = await ask(recipe, JSON.stringify({
-        prompt,
-        urls,
-    }), true);
+export async function evaluate_urls(
+	prompt: string,
+	urls: string[],
+): Promise<string[]> {
+	const recipe = (await import(`./prompts/url_evaluation.md?raw`)).default;
 
-    return JSON.parse(response).urls;
+	const response = await ask(
+		recipe,
+		JSON.stringify({
+			prompt,
+			urls,
+		}),
+		{ json: true },
+	);
+
+	return JSON.parse(response).urls;
 }
 
 export async function improve_user_request(request: string): Promise<string> {
-    const recipe = (await import(`./prompts/improve_user_request.md?raw`)).default
+	const recipe = (await import(`./prompts/improve_user_request.md?raw`))
+		.default;
 
-    return await ask(recipe, request)
+	return await ask(recipe, request);
 }
 
-export async function website_search_summary(prompt: string, website: string): Promise<string> {
-    const recipe = (await import(`./prompts/website_search_summary.md?raw`)).default
+export async function website_search_summary(prompt: string, website: string) {
+	const recipe = (await import(`./prompts/website_search_summary.md?raw`))
+		.default;
 
-    return await ask(recipe, JSON.stringify({
-        prompt,
-        website
-    }))
+	return await ask_stream(
+		recipe,
+		JSON.stringify({
+			prompt,
+			website,
+		}),
+	);
 }
 
-export async function extract_info_json(prompt: string, text: string): Promise<string[]> {
-    const recipe = (await import(`./prompts/extract_info_json.md?raw`)).default
+export async function extract_info_json(
+	prompt: string,
+	text: string,
+): Promise<string[]> {
+	const recipe = (await import(`./prompts/extract_info_json.md?raw`)).default;
 
-    const response = await ask(recipe, JSON.stringify({
-        prompt,
-        text
-    }), true);
+	const response = await ask(
+		recipe,
+		JSON.stringify({
+			prompt,
+			text,
+		}),
+		{ json: true },
+	);
 
-    return JSON.parse(response).results
+	return JSON.parse(response).results;
 }
