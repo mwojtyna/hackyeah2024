fae78a90633ab878e1f0ea28aeabd09f8a9022ad
Mateusz Wojtyna <w_mateusz@hotmail.com>
2024-09-28 16:02:00 +0200

    feat: responsive horizontal and vertical ui and webview layout

--- src/backend/main.ts
+++ src/backend/main.ts
@@ -1,4 +1,5 @@
 import { app, BrowserWindow, WebContentsView } from "electron";
+import { BaseWindow } from "electron/main";
 import path from "path";
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
@@ -6,11 +7,12 @@ if (require("electron-squirrel-startup")) {
     app.quit();
 }
 
-function createWindow() {
-    const WIDTH = 800;
-    const HEIGHT = 800;
-    const UI_HEIGHT = 300;
+const WIDTH = 1420;
+const HEIGHT = 800;
+const UI_HORI_SIZE = 500;
+const UI_VERT_SIZE = 300;
 
+function createWindow() {
     // Create the browser window.
     const mainWindow = new BrowserWindow({
         width: WIDTH,
@@ -22,20 +24,38 @@ function createWindow() {
 
     const uiView = new WebContentsView();
     mainWindow.contentView.addChildView(uiView);
-    uiView.setBounds({
-        x: 0,
-        y: mainWindow.getSize()[1] - UI_HEIGHT,
-        width: mainWindow.getSize()[0],
-        height: UI_HEIGHT,
-    });
-    mainWindow.on("resize", () =>
+    if (landscape(mainWindow, UI_HORI_SIZE)) {
         uiView.setBounds({
             x: 0,
-            y: mainWindow.getSize()[1] - UI_HEIGHT,
+            y: 0,
+            width: UI_HORI_SIZE,
+            height: mainWindow.getSize()[1],
+        });
+    } else {
+        uiView.setBounds({
+            x: 0,
+            y: mainWindow.getSize()[1] - UI_VERT_SIZE,
             width: mainWindow.getSize()[0],
-            height: UI_HEIGHT,
-        }),
-    );
+            height: UI_VERT_SIZE,
+        });
+    }
+    mainWindow.on("resize", () => {
+        if (landscape(mainWindow, UI_HORI_SIZE)) {
+            uiView.setBounds({
+                x: 0,
+                y: 0,
+                width: UI_HORI_SIZE,
+                height: mainWindow.getSize()[1],
+            });
+        } else {
+            uiView.setBounds({
+                x: 0,
+                y: mainWindow.getSize()[1] - UI_VERT_SIZE,
+                width: mainWindow.getSize()[0],
+                height: UI_VERT_SIZE,
+            });
+        }
+    });
 
     // and load the index.html of the app.
     // @ts-expect-error missing types for env
@@ -52,23 +72,45 @@ function createWindow() {
     const embedView = new WebContentsView();
     mainWindow.contentView.addChildView(embedView);
     embedView.webContents.loadURL("https://google.pl");
-    embedView.setBounds({
-        x: 0,
-        y: 0,
-        width: mainWindow.getSize()[0],
-        height: mainWindow.getSize()[1] - UI_HEIGHT,
-    });
-    mainWindow.on("resize", () =>
+    if (landscape(mainWindow, UI_HORI_SIZE)) {
+        embedView.setBounds({
+            x: UI_HORI_SIZE,
+            y: 0,
+            width: mainWindow.getSize()[0] - UI_HORI_SIZE,
+            height: mainWindow.getSize()[1],
+        });
+    } else {
         embedView.setBounds({
             x: 0,
             y: 0,
             width: mainWindow.getSize()[0],
-            height: mainWindow.getSize()[1] - UI_HEIGHT,
-        }),
-    );
+            height: mainWindow.getSize()[1] - UI_VERT_SIZE,
+        });
+    }
+    mainWindow.on("resize", () => {
+        if (landscape(mainWindow, UI_HORI_SIZE)) {
+            embedView.setBounds({
+                x: UI_HORI_SIZE,
+                y: 0,
+                width: mainWindow.getSize()[0] - UI_HORI_SIZE,
+                height: mainWindow.getSize()[1],
+            });
+        } else {
+            embedView.setBounds({
+                x: 0,
+                y: 0,
+                width: mainWindow.getSize()[0],
+                height: mainWindow.getSize()[1] - UI_VERT_SIZE,
+            });
+        }
+    });
 
     // Open the DevTools.
     // mainWindow.webContents.openDevTools();
+
+    function landscape(window: BaseWindow, uiSize: number): boolean {
+        return uiSize < window.getSize()[0] / 2;
+    }
 }
 
 // This method will be called when Electron has finished
@@ -80,9 +122,7 @@ app.on("ready", createWindow);
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
-        app.quit();
-    }
+    app.quit();
 });
 
 app.on("activate", () => {
