21953a7a61994b6ff54fe0139225a5f1b87fa42b
Borys <borysjb@gmail.com>
2024-09-28 18:59:22 +0200

    record works, but unsafe

--- record.js
+++ record.js
@@ -2,36 +2,44 @@ const fs = require("fs");
 const { WaveFile } = require("wavefile");
 const { PvRecorder } = require("@picovoice/pvrecorder-node");
 
-let stop = false;
-const sleep = delay => new Promise(resolve => setTimeout(resolve, delay))
+const { program } = require("commander");
+
+const sleep = delay => new Promise(resolve => setTimeout(resolve, delay));
+
+// this needs to be done not by changing a global variable
+let stopAudioRecording = false;
+
 
 async function record() {
-	let outputpath = "audio.wav";
-
-	const wav = new WaveFile();
 	const frames = [];
 	const framelength = 512;
+	//console.log(PvRecorder.getAvailableDevices());
+	const wav = new WaveFile();
+	const recorder = new PvRecorder(framelength, -1);
+	recorder.start()
 
-	const recorder = new PvRecorder(framelength, 1);
-
-	while(!stop) {
+	while(!stopAudioRecording) {
 		const frame = await recorder.read();
 		frames.push(frame);
 	}
 
 	const audiodata = new Int16Array(recorder.frameLength * frames.length);
 	for (let i = 0; i < frames.length; i++) {
-		audiodata.set(frames[i], i*recorder.frameLength);
+		audiodata.set(frames[i], i * recorder.frameLength);
 	}
-	wav.fromScratch(1,recorder.sampleRate, '16', audioData);
-	fs.writeFileSync(outputpath, wav.tobuffer());
-
+	wav.fromScratch(1, recorder.sampleRate, '16', audiodata);
+	fs.writeFileSync(`recordedAudio.wav`, wav.toBuffer());
+	recorder.stop()
+	//console.log("stopped recording");
 	recorder.release();
 }
 
-record();
-
-sleep(5000);
-
-stop=true;
-
+(async function () {
+	try {
+		record();
+		await sleep(5000);
+		stopAudioRecording=true;
+		// change this to some mutex thing
+	}
+	catch {}
+})();
