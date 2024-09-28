4bfa868eef640656917eebf5d40270e444808e92
Borys <borysjb@gmail.com>
2024-09-28 16:41:35 +0200

    recorder kinda works

---
+++ record.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const { WaveFile } = require("wavefile");
+const { PvRecorder } = require("@picovoice/pvrecorder-node");
+
+let stop = false;
+
+async function record() {
+	let outputpath = "audio.wav";
+
+	const wav = new WaveFile();
+	const frames = [];
+	const framelength = 512;
+
+	const recorder = new PvRecorder(framelength, -1);
+
+	while(!stop) {
+		const frame = await recorder.read();
+		frames.push(frame);
+	}
+
+	const audiodata = new Int16Array(recorder.frameLength * frames.length);
+	for (let i = 0; i < frames.length; i++) {
+		audiodata.set(frames[i], i*recorder.frameLength);
+	}
+	wav.fromScratch(1,recorder.sampleRate, '16', audioData);
+	fs.writeFileSync(outputpath, wav.tobuffer());
+
+	recorder.release();
+}
+
+
